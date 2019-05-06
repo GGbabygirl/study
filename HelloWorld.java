6bca270e08e5303506003e6276f203b6cf6b5187
yourname <yourname@me.com>
2019-05-06 10:35:42 +0800

    在master分支下增加一条输出语句

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("修改了HelloWorld类");
+               System.out.println("在master分支下输出一条信息");
 	}
 }
