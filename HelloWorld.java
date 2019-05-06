b24c946ea6bf1b5d783520883458eff2a157bc2b
yourname <yourname@me.com>
2019-05-06 10:48:17 +0800

    在develop分支下添加一条输出语句

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("修改了HelloWorld类");
+                System.out.println("在Develop分支下添加一条输出语句");
 	}
 }
